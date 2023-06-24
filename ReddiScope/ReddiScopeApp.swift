6503475ee33f5a87715b8d0cb5d7945888b5bdcc
David Bureš <buresdv@gmail.com>
2023-06-24 13:34:58 +0200

    Initial Commit

---
+++ ReddiScope/ReddiScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReddiScopeApp.swift
+//  ReddiScope
+//
+//  Created by David Bureš on 24.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ReddiScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
